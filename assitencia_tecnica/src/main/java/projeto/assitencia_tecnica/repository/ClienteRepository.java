1d47a590f9a19e103a57399423e8b9319d972123
Eric <ericbm30@outlook.com>
2022-11-18 00:13:17 -0300

    Initial commit

--- assitencia_tecnica/src/main/java/projeto/assitencia_tecnica/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package projeto.assitencia_tecnica.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import projeto.assitencia_tecnica.models.Cliente;
-
-public interface ClienteRepository extends CrudRepository<Cliente, Long> {
-    Cliente findById(long id);
-}
--- assitencia_tecnica/src/main/java/projeto/assitencia_tecnica/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package projeto.assitencia_tecnica.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import projeto.assitencia_tecnica.models.Produto;
-
-public interface ProdutoRepository extends CrudRepository<Produto, Long> {
-}
